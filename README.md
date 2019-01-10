8a0f69cbab5cafb1a4c1e3b0cebc117ca55212e1
Arun Vavilakolanu <vv.arun@ymail.com>
2019-01-10 13:09:01 +0530

    Created file_organizer.py

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # HandyScripts
 Some Handy Python scripts to be used whenever, wherever and however you want.
 
----
+*** 
 #### file_copy.py
 This File copies files from one folder to another.
+
+#### file_organizer.py
+This file organizes files into respective folders.
+The folder names and file types can be specified in the
+given python dictionary.
---
+++ file_organizer.py
@@ -0,0 +1,54 @@
+import os
+
+
+class Organize:
+    # consider = [".jpg", ".png", ".bmp", ".py", ".lnk", ".txt", ".rtf"]
+    refer = {
+        "Images": [".jpg", ".png", ".bmp", ".jpeg"],
+        "Python Files": [".py", ".pyw"],
+        "Documents": [".txt", ".rtf", ".docx", ".doc"],
+        "Compressed": ["."]
+    }
+
+    assign = {
+        "Images": [],
+        "Python Files": [],
+        "Documents": []
+    }
+
+    def __init__(self, main_path):
+        self.main_path = main_path
+
+    def get_names(self):
+        path = self.main_path
+        d = self.refer
+        fin = self.assign
+        print(self.main_path)
+        for filename in os.listdir(os.path.abspath(path)):
+            base_file, ext = os.path.splitext(filename)
+            for x in d.keys():
+                if ext in d[x]:
+                    print("'" + filename + "'", "belongs in", x, "Folder")
+                    fin[x].append(filename)
+        print(fin)
+
+    def arrange(self):
+        path = self.main_path
+        fin = self.assign
+        # create Folders
+        for x in fin.keys():
+            try:
+                os.mkdir(path + x)
+            except OSError:
+                print("Creation of the directory %s failed" % path)
+
+        for y in fin.keys():
+            if len(fin[y]) > 0:
+                for file in fin[y]:
+                    os.rename(path+file, path + y + "\\" + file)
+
+
+o = Organize("C:\\Users\\vvaru\\Desktop\\Test folder\\")
+
+o.get_names()
+o.arrange()
